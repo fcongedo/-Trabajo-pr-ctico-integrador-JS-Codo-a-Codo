76055d3d32378d011f7fc69d63efe774327c3a86
fcongedo <franco_congedo@hotmail.com>
2023-11-01 01:20:23 -0300

    validacion de datos ingresados

--- script/script.js
+++ script/script.js
@@ -1,3 +1,42 @@
+let validNameRegex = /^[A-Za-zÁáÉéÍíÓóÚúÑñ]+$/;
+let validEmailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+let validateEmail = (email) => {
+  return validEmailRegex.test(email);
+};
+
+let validateName = (name) => {
+  return validNameRegex.test(name) && name.trim() !== '';
+};
+
+let validateFields = () => {
+  const camposDeEntrada = [nombre, surname, email, cantidad];
+  let hasError = false;
+
+  camposDeEntrada.forEach(input => {
+    input.style.borderColor = "";
+
+    if (input === cantidad) {
+      if (input.value.trim() === '' || parseFloat(input.value) <= 0) {
+        input.style.borderColor = "red";
+        hasError = true;
+      }
+    } else if (input === email) {
+      if (!validateEmail(input.value)) {
+        input.style.borderColor = "red";
+        hasError = true;
+      }
+    } else {
+      if (!validateName(input.value)) {
+        input.style.borderColor = "red";
+        hasError = true;
+      }
+    }
+  });
+
+  return !hasError;
+};
+
 let select = document.querySelector("#select");
 let divTotal = document.querySelector(".total");
 let cantidad = document.querySelector(".cantidad");
@@ -20,37 +59,10 @@ let total = (cantidad, categoria, div) => {
   }
 };
 
-let emptyInput = (inputs) => {
-  let hasError = false;
-
-  inputs.forEach(input => {
-    if (input.value === "") {
-      input.style.borderColor = "red";
-      hasError = true;
-    } else {
-      input.style.borderColor = "green";
-    }
-  });
-
-  return hasError;
-};
-
 resume.addEventListener("click", (e) => {
   e.preventDefault();
 
-  // Obtén todos los campos de entrada en el formulario
-  const camposDeEntrada = [nombre, surname, email, cantidad];
-
-  // Verifica campos vacíos utilizando la función modified emptyInput
-  let hasError = emptyInput(camposDeEntrada);
-
-  if (hasError) {
-    Swal.fire({
-      icon: "error",
-      title: "Por favor, complete todos los campos obligatorios",
-    });
-  } else {
-    // No hay campos vacíos, continúa con el proceso
+  if (validateFields()) {
     Swal.fire({
       icon: "success",
       title: "Gracias por realizar tu compra",
@@ -62,6 +74,11 @@ resume.addEventListener("click", (e) => {
       if (result.isConfirmed) {
         // Aquí puedes realizar otras acciones después de completar el proceso
       }
+    });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Por favor, complete todos los campos obligatorios correctamente",
     });
   }
 });
@@ -77,11 +94,10 @@ cantidad.addEventListener("input", (e) => {
   total(cantidad.value, select.value, divTotal);
 });
 
-// Agrega un manejador de eventos al botón "Borrar" para restablecer los estilos
 const botonBorrar = document.querySelector('input[type="reset"]');
 botonBorrar.addEventListener("click", function() {
   const camposDeEntrada = [nombre, surname, email, cantidad];
   camposDeEntrada.forEach(function(input) {
-    input.style.borderColor = ""; // Restablece el color de borde
+    input.style.borderColor = "";
   });
 });
